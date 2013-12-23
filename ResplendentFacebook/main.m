097ac5745ac1cdc71637af6a5c8fccc47d842587
BenMaer <ben@resplendent.co>
2013-12-23 16:09:10 -0500

    added project

---
+++ ResplendentFacebook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ResplendentFacebook
+//
+//  Created by Benjamin Maer on 12/23/13.
+//  Copyright (c) 2013 Resplendent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RFBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RFBAppDelegate class]));
+    }
+}
